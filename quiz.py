b5bd5813273bb471ceafbe9f2e15eb72c65faa51
zperez2 <zperez2@babson.edu>
2017-03-21 09:50:54 -0400

    Finish Quiz

--- quiz.py
+++ quiz.py
@@ -10,22 +10,36 @@ only if both are negative.
 """
 
 
-def pos_neg(a, b, negative):
-    pass
-
-
-# Expected outputs:
-
-print(pos_neg(1, -1, False))
-# True
-print(pos_neg(-1, 1, False))
-# True
-print(pos_neg(-4, -5, True))
-# True
-print(pos_neg(-2, -5, False))
-# False
-print(pos_neg(1, 2, False))
-# False
+# def pos_neg(a, b, negative):
+#     a = int(input("Enter a number between -10 and 10"))
+#     b = int(input("Enter a number between -10 and 10"))
+#     if a > 0 or b > 0:
+#         negative = False
+#         return(a, b, negative)
+#         return True
+#     if a > 0 and b > 0:
+#         negative = False
+#         return(a, b, negative)
+#         return False
+#     if a < 0 and b < 0:
+#         negative = True
+#         return(a, b, negative)
+#         return False
+#     pass
+# #
+#
+# # Expected outputs:
+#
+# print(pos_neg(1, -1, False))
+# # True
+# print(pos_neg(-1, 1, False))
+# # True
+# print(pos_neg(-4, -5, True))
+# # True
+# print(pos_neg(-2, -5, False))
+# # False
+# print(pos_neg(1, 2, False))
+# # False
 
 
 """
@@ -44,9 +58,16 @@ years (for example, 2000).
 
 """
 
-
-def leap_year(year):
-    pass
+#
+# def leap_year(year):
+#     year = int(input("What is the year?"))
+#     while year % 4 or year % 400:
+#             return("Leap Year!")
+#     if year % 100:
+#             return("Not Leap Year.")
+#
+#
+#     pass
 
 
 # When you've completed your function, uncomment the
@@ -72,7 +93,14 @@ Write a function with loops that computes the sum of all squares between
 
 
 def sum_squares(n):
-    pass
+    n = 0
+
+for i in range (1000):
+    if i % 2 == 0:
+        print("n=", n, "i=",i)
+        s = s + i
+print(s)
+    # pass
 
 # When you've completed your function, uncomment the
 # following lines and run this file to test!
